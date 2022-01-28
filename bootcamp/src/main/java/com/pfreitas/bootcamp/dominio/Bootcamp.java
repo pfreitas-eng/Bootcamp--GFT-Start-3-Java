67f4d0a0f42311be813d53cdd061416d1bfe288b
Pedro Freitas <pfreitas.eng@gmail.com>
2022-01-28 15:50:52 -0300

    Classes Dev e Bootcamp

---
+++ bootcamp/src/main/java/com/pfreitas/bootcamp/dominio/Bootcamp.java
@@ -0,0 +1,30 @@
+package com.pfreitas.bootcamp.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Bootcamp {
+    
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public Bootcamp(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+}
---
+++ bootcamp/src/main/java/com/pfreitas/bootcamp/dominio/Dev.java
@@ -0,0 +1,25 @@
+package com.pfreitas.bootcamp.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Dev {
+    
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){}
+
+}
