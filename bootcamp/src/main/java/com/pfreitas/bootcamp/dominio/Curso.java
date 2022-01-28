39d0f1136dab3991a94b052ed44e30ca3dd48115
Pedro Freitas <pfreitas.eng@gmail.com>
2022-01-28 15:12:53 -0300

    Classes Curso e Mentoria e seus atributos

---
+++ bootcamp/src/main/java/com/pfreitas/bootcamp/dominio/Curso.java
@@ -0,0 +1,9 @@
+package com.pfreitas.bootcamp.dominio;
+
+public class Curso {
+    
+    String titulo;
+    String descricao;
+    int cargaHoraria;
+
+}
---
+++ bootcamp/src/main/java/com/pfreitas/bootcamp/dominio/Mentoria.java
@@ -0,0 +1,11 @@
+package com.pfreitas.bootcamp.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    
+    String titulo;
+    String descricao;
+    LocalDate data;
+
+}
